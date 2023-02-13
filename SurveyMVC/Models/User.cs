75fbb1f775b7ac85dd0bd9d4b682bae3de849f68
joshthecomposer <110321887+joshthecomposer@users.noreply.github.com>
2023-02-12 23:50:17 -0500

    changing to mac

--- SurveyMVC/Models/User.cs
+++ SurveyMVC/Models/User.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SurveyMVC.Models;
+
 
 public class User
 {
+    [Required]
     public string? Name { get; set; }
+    [Required]
     public string? Location { get; set; }
+    [Required]
     public string? Language { get; set; }
+    [MaxLength(255)]
     public string? Message { get; set; }
 }
