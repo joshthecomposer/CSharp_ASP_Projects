5f063711da84202b0ec15c9b24e06e1533d4c2e0
joshthecomposer <110321887+joshthecomposer@users.noreply.github.com>
2023-02-12 22:45:52 -0500

    playing with layouts and components

---
+++ ViewModelFun/Controllers/MainController.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ViewModelFun.Models;
+
+namespace ViewModelFun.Controllers;
+
+public class MainController : Controller
+{
+    private readonly ILogger<MainController> _logger;
+
+    public MainController(ILogger<MainController> logger)
+    {
+        _logger = logger;
+    }
+    [HttpGet("")]
+    public ViewResult Index()
+    {
+        string message = "Hello there, this is an striiiing";
+        return View("Index", message);
+    }
+    [HttpGet("/numbers")]
+    public ViewResult Numbers()
+    {
+        List<int> numbers = new List<int>{1, 10, 15, 2, 45};
+        return View("Numbers", numbers);
+    }
+
+    [HttpGet("/user")]
+    public ViewResult ViewUser()
+    {
+        User user = new User();
+        user.FirstName = "John";
+        user.LastName = "Meme";
+        return View("ViewUser", user);
+    }
+
+    [HttpGet("/users")]
+    public ViewResult ViewUsers()
+    {
+        List<User> users = new List<User>
+        {
+            new User() { FirstName = "dan", LastName = "theman"},
+            new User() {FirstName="bill", LastName="swill"}
+        };
+        return View("ViewUsers", users);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ ViewModelFun/Models/User.cs
@@ -0,0 +1,8 @@
+public class User
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    
+    public User(){}
+    public User(string? firstName, string? lastName){}
+}
