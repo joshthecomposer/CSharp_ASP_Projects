0cc6bdcb0a86e6e9e82739ca0289d28a80947923
Joshua Wise <joshuawise@joshuas-mbp.myfiosgateway.com>
2023-02-12 20:17:41 -0500

    init

---
+++ FirstMVC/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using FirstMVC.Models;
+
+namespace FirstMVC.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet("second")]
+    public ViewResult SecondPage()
+    {
+        return View();
+    }
+}
---
+++ FirstMVC/Models/Friend.cs
@@ -0,0 +1,7 @@
+namespace FirstMVC.Models;
+public class Friend
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+}
